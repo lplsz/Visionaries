1730173865886ec315ef286d6d7b59a322631f79
Echooooo-zhn <haonanZHONG17@outlook.com>
2022-06-24 11:51:01 +1000

    initiate home page and error alert

---
+++ frontend/src/component/ErrorSnackBar.jsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
+
+export default function ErrorSnackbar(props) {
+  
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    props.setOpen(false);
+  };
+
+  const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={20} ref={ref} variant="filled" {...props} />;
+  });
+  return (
+    <div>
+      <Snackbar
+        open={props.open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+      >
+      
+      
+        <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+          {props.message}
+        </Alert>
+      </Snackbar>
+    </div>
+  );
+}
