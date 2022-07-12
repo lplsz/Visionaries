65dd8a35e648d5107ee546816159b2315c2d5fd0
Skylar <s.m.@SkylardeMBP.lan>
2022-07-12 22:57:25 +1000

    thread update,having tested yet

--- backend/wellbeing/thread/views.py
+++ backend/wellbeing/thread/views.py
@@ -4,7 +4,7 @@ from flask_jwt_extended import jwt_required
 from wellbeing.thread.schemas import (GetThreadOutSchema)
 import wellbeing.thread.controllers as controllers
 
-thread_blueprint = APIBlueprint('Thread', __name__)
+thread_blueprint = APIBlueprint('thread', __name__)
 
 @thread_blueprint.route('/thread/<int:qa_id>')
 class ThreadByID(MethodView):
